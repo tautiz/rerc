1e59a383ba50b7ee5d87185938dc756db5891db9
Tautvydas <tautvydas@kavosdraugas.lt>
2018-01-03 16:45:08 +0200

    Added Slack notifications

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var http = require("http");
 var https = require("https");
 var request = require('request');
+var Slack = require('slack-node');
 
 let EURBTC = {};
 let BTCEUR = {};
@@ -11,6 +12,15 @@ let OLD_EUR = 2600;
 let EUR = 2077.31;
 let OLD_BTC = 0.16902117;
 let BTC = 0.1706694;
+
+
+
+apiToken = "xoxp-293583170194-293724356261-293065599729-7f1816b11bfd496475b97af3cfadd9fb";
+slack = new Slack(apiToken);
+slack.api("users.list", function(err, response) {
+    console.log(response);
+});
+
 
 request('https://www.revolut.com/api/quote/internal/EURBTC', function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -25,6 +35,13 @@ request('https://www.revolut.com/api/quote/internal/BTCEUR', function (error, re
         console.log(BTCEUR.rate)      
     }
 })
+
+slack.api('chat.postMessage', {
+    text:'hello from nodejs',
+    channel:'#general'
+}, function(err, response){
+    console.log(response);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
