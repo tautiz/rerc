f0ada8b4e62c8023ecf62175ab966cfb33c95c5f
Tautvydas <tautvydas@kavosdraugas.lt>
2018-01-03 19:48:18 +0200

    Updates

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,22 @@ slack.api('users.list', function (err, response) {
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    db.createCollection("customers", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+});
+
+
 function makeRequest()
 {
     current_time_stamp = Date.now();
@@ -53,14 +69,8 @@ function makeRequest()
     })
 
     MongoClient.connect(url, function(err, db) {
-        if (err) {
-            db.createCollection("customers", function(err, res) {
-                if (err) throw err;
-                console.log("Collection created!");
-                db.close();
-            });
-            throw err
-        }
+        if (err) throw err
+
         var myobj = { BTCEUR: BTCEUR, EURBTC: EURBTC, current_time_stamp: current_time_stamp };
 
         db.collection("customers").insertOne(myobj, function(err, res) {
